895bedde6dfad2c7f120955cff0775b8906dd050
student <student@ip-172-31-52-238.us-west-2.compute.internal>
2021-10-20 19:55:56 -0700

    Robot races(IP)

--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -7,33 +7,45 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _02_RobotRace {
-    // 1. make a main method
-public static void main(String[]args) {
-	Robot[] robots= new Robot[4];
-	for(int i = 0;i<robots.length;i++) {
-		int spaceX=0;
-		robots[i]=new Robot();
-		robots[i].setX(40+spaceX);
-		robots[i].setY(30);
-		spaceX +=20;
+	// 1. make a main method
+	public static void main(String[] args) {
+		int SpaceX = 0;
+		Robot[] robots = new Robot[5];
+		Random r = new Random();
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+			robots[i].setWindowSize(1000, 400);
+			robots[i].setX(50 + SpaceX);
+			robots[i].setY(350);
+			SpaceX += 175;
+		}
+		while(robots[0].getY()>50) {
+			
+		for(int i =0; i<robots.length;i++) {
+        	
+        		robots[i].move(r.nextInt(50));
+        	
+        	
+        }
+		}
+		
 	}
-}
-        // 2. create an array of 5 robots.
+	// 2. create an array of 5 robots.
+	
+	// 3. use a for loop to initialize the robots.
 
-        // 3. use a for loop to initialize the robots.
+	// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-        // 4. make each robot start at the bottom of the screen, side by side, facing up
-    
-        // 5. use another for loop to iterate through the array and make each robot move
-        // a random amount less than 50.
-    
-        // 6. use a while loop to repeat step 5 until a robot has reached the top of the
-        // screen.
-    
-        // 7. declare that robot the winner and throw it a party!
-    
-        // 8. try different races with different amounts of robots.
-    
-        // 9. make the robots race around a circular track.
+	// 5. use another for loop to iterate through the array and make each robot move
+	// a random amount less than 50.
+
+	// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+	// screen.
+
+	// 7. declare that robot the winner and throw it a party!
+
+	// 8. try different races with different amounts of robots.
+
+	// 9. make the robots race around a circular track.
 
 }
